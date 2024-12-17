5899ae8d65367aff175894f9b7a50bd6412fa851
KarthiknV11 <karthikeyan.venu11@gmail.com>
2024-12-17 09:50:05 +0530

    Add files via upload
    Day 1

---
+++ day1/FireFox.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class FireFox {
+
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int releaseYear = 1998;
+		String browserName = "FireFox";
+		Float browserVersion = 100.2f;
+		char browserLogo = 'f';
+		Boolean isVisible = true;
+		
+		System.out.println("Browser Name is :" +browserName + "\n" + "Browser Version is :" +browserVersion + "\n" + "Release Year :" +releaseYear + "\n" + "Browser Logo is :" +browserLogo +"\n" + "Check whether f is visible or not :" +isVisible);
+
+	}
+
+}
